6f407ddeb4d02c35cfe0d2db4728b5c4f38b9dc1
huongnguye1234 <huongnguyenthihuong1234@gmail.com>
2020-02-26 20:38:43 +0700

    update

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const routes = require("./router");
+const app = express();
+const port = 3001;
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+// set folter contain files view
+app.set("views", path.join(__dirname, "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/question", function(req, res) {
+  var options = {
+    root: path.join(__dirname, "public")
+  };
+  res.render("/question", options);
+});
+
+app.use(express.static("./public"));
+app.use("/router", routes);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
