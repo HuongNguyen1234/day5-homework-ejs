11ed8bec0ed7c411d75955ab3312e782729ba081
huongnguye1234 <huongnguyenthihuong1234@gmail.com>
2020-02-27 13:09:17 +0700

    done

--- api.js
+++ api.js
@@ -1,32 +1,84 @@
 const express = require("express");
 const fs = require("fs");
-
+const fileController = require("./fileController");
 const router = express.Router();
 
 router
-  .route("/question")
+  .route("/addQues")
   .get(function(req, res) {
-    fs.readFile("./questions.txt", "UTF-8", function(err, data) {
-      if (err) {
-        res.end(err);
-      } else {
-        const questions = data.split(/\r?\n/);
-        const randomQuestion =
-          questions[Math.floor(Math.random() * questions.length)];
-
-        // Render view question.ejs to show question
-        res.render("question", { question: randomQuestion });
-      }
-    });
+    res.render("addQues");
   })
   .post(function(req, res) {
-    fs.writeFile("./questions.txt", req.body.question, function(err, data) {
-      if (err) {
-        res.end("Đã có lỗi xảy ra, vui lòng thử lại");
-      } else {
-        res.end("Tạo câu hỏi thành công");
-      }
-    });
+    try {
+      let data = fileController.readFileSync("./data.json");
+      let id = data.length + 1;
+      data.push({
+        id: id,
+        questionContent: req.body.question,
+        yes: 0,
+        no: 0
+      });
+      fileController.writeFile("./data.json", data, err => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect("/");
+      });
+    } catch (ex) {
+      console.log(ex);
+    }
   });
 
+router.get("/", function(req, res) {
+  try {
+    let questions = [...fileController.readFileSync("./data.json")];
+    if (questions.length > 0 && typeof questions !== undefined) {
+      let question =
+        questions[Math.floor(Math.random() * questions.length) + 0];
+      res.render("home", {
+        question: question.questionContent,
+        id: question.id,
+        yes:
+          "<a href='/vote/" +
+          question.id +
+          "/1'><button type='button' name='btnYes' value='1'>Yes</button></a>",
+        no:
+          "<a href='/vote/" +
+          question.id +
+          "/0'><button type='button' name='btnNo' value='0'>No</button></a>"
+      });
+    } else {
+      res.render("home", { question: "the question list is empty" });
+    }
+  } catch (ex) {
+    console.log(ex);
+  }
+});
+router.get("/vote/:id/:value", function(req, res) {
+  try {
+    var data = fileController.readFileSync("./data.json");
+    let id = req.params.id;
+    let value = req.params.value;
+    data.forEach(element => {
+      if (element.id == id) {
+        if (req.params.value == 1) {
+          element.yes = Number(element.yes + 1);
+        } else {
+          element.no = Number(element.no + 1);
+        }
+      }
+    });
+    console.log(data);
+    fileController.writeFile("./data.json", data, err => {
+      if (err) {
+        console.log(err);
+      }
+      res.render("vote", {
+        data: data
+      });
+    });
+  } catch (ex) {
+    console.log(ex);
+  }
+});
 module.exports = router;
--- app.js
+++ app.js
@@ -5,23 +5,14 @@ const path = require("path");
 const api = require("./api");
 const app = express();
 const port = 3001;
-
+const fileController = require("./fileController");
 // set the view engine to ejs
 app.set("view engine", "ejs");
 // set folter contain files view
 app.set("views", path.join(__dirname, "/public"));
-
+app.set("view options", { layout: "main.ejs" });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.get("/question", function(req, res) {
-  var options = {
-    root: path.join(__dirname, "public")
-  };
-  res.render("question", options);
-});
-
 app.use(express.static("./public"));
-app.use("/api", api);
-
+app.use("/", api);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
